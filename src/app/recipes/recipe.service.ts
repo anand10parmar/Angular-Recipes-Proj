38bb25e998efbaec1544ed38c035d8a6f35f7bbe
anand <anand10parmar@gmail.com>
2018-06-27 14:44:42 -0400

    first

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Allo Paratha',
+      'Soft dough stuffed with the spicy filling. yummy!!',
+      'https://i.imgur.com/23vTuzt.png',
+      [
+        new Ingredient('Potato', 1),
+        new Ingredient('Onion', 1)
+      ]),
+    new Recipe('Pav Bhaji',
+      'Vegetable curry, fried and served with a soft bread rol',
+      'https://i.imgur.com/LtciHvA.png',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Bhaji', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
